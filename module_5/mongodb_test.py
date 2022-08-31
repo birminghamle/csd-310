3b030f824c8b8cb016aefc1b18d3d1dc468ac298
Lathan <lbirmingham@my365.bellevue.edu>
2022-08-30 21:38:16 -0400

    Push #2.

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,18 +1,14 @@
 """ 
     Title: mongodb_test.py
-    Author: Lathan
+    Author: Lathan Birmingham
     Date: 30 August 2022
     Description: Test program for connecting to a 
                  MongoDB Atlas cluster
 """
 
 from pymongo import MongoClient
- 
 url = "mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority"
-
 client = MongoClient(url)
-
 db = client.pytech
-
 print("\n -- Pytech COllection List --")
 print(db.list_collection_names())
